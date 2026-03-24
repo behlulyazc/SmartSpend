da496a3bbfef0ea3ac87cd397faea874ba062af7
Behlül YAZICI <166131990+behlulyazc@users.noreply.github.com>
2026-03-24 14:53:19 +0300

    feat: Terminal üzerinden dinamik veri girişi (input) özelliği eklendi

--- main.py
+++ main.py
@@ -35,8 +35,27 @@ def butce_kontrol(df):
             else:
                 print(f"{kat} bütçe dahilinde. (Kalan: {limit-miktar} TL)")
 
-if __name__ == "__main__":
+def yeni_harcama_ekle(dosya_yolu):
+    print("\n--- 📝 Yeni Harcama Girişi ---")
+    tarih = input("Tarih (YYYY-MM-DD formatında): ")
+    kategori = input("Kategori (Gıda, Eğitim, Kıyafet vb.): ")
+    miktar = input("Miktar (Harcanan miktare TL şeklinde ): ")
+    acıklama = input("Kısa bir açıklama: ")
+
+    yeni_satir = f"\n{tarih},{kategori},{miktar},{acıklama}"
+
+    with open(dosya_yolu, "a", encoding="utf-8") as f:
+        f.write(yeni_satir)
     
-    veriler = veriyi_yukle('harcamalar.csv')
+    print(f" Başarılı: {kategori} kategorisindeki {miktar} TL'lik harcama kaydedildi!")
+
+if __name__ == "__main__":
+    dosya = 'harcamalar.csv'
+    
+    secim = input("Yeni harcama eklemek ister misin? (e/h): ").lower()
+    if secim == 'e':
+        yeni_harcama_ekle(dosya)
+    
+    veriler = veriyi_yukle(dosya)
     harcamalari_analiz_et_ve_ciz(veriler)
     butce_kontrol(veriler)
